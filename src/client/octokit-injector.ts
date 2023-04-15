9efa463a3d416b50aea936b05b1499bdb9c9c6b5
Jinseok Eo <biud436@gmail.com>
2023-04-16 08:00:00 +0900

    feat: added prettier

--- src/client/octokit-injector.ts
+++ src/client/octokit-injector.ts
@@ -9,94 +9,96 @@ const PER_PAGE = 100;
 const HTTP_STATUS_OK = 200;
 
 const DEFAULT_OCTOKIT_HEADER = {
-  headers: {
-    "X-GitHub-Api-Version": "2022-11-28",
-  },
+    headers: {
+        "X-GitHub-Api-Version": "2022-11-28",
+    },
 };
 
 @injectable()
 export class OctokitInjector {
-  private octokit!: Octokit;
+    private octokit!: Octokit;
 
-  private followers?: User[];
-  private following?: Following[];
+    private followers?: User[];
+    private following?: Following[];
 
-  constructor(
-    @inject(CONFIG_SERVICE) private readonly configService: ConfigService
-  ) {}
+    constructor(
+        @inject(CONFIG_SERVICE) private readonly configService: ConfigService
+    ) {}
 
-  async create() {
-    try {
-      this.octokit = new Octokit({
-        auth: this.configService.get("GITHUB_TOKEN"),
-      });
+    async create() {
+        try {
+            this.octokit = new Octokit({
+                auth: this.configService.get("GITHUB_TOKEN"),
+            });
 
-      this.followers = await this.request("GET /user/followers");
-      this.following = await this.request("GET /user/following");
-      const compareResult = await this.compareFollowingAndFollowers();
+            this.followers = await this.request("GET /user/followers");
+            this.following = await this.request("GET /user/following");
+            const compareResult = await this.compareFollowingAndFollowers();
 
-      App.Listeners.emit("result", compareResult);
-    } catch (e: any) {}
-  }
-
-  async request(url: "GET /user/followers" | "GET /user/following") {
-    let currentPage = 1;
-
-    const maxPage = this.configService.get<number>("MAX_PAGE");
-    let data: Array<User> = [];
-
-    while (currentPage <= maxPage) {
-      try {
-        const res = await this.octokit.request(url, {
-          DEFAULT_OCTOKIT_HEADER,
-          per_page: PER_PAGE,
-          page: currentPage,
-        });
-
-        if (res.status === HTTP_STATUS_OK) {
-          data = [
-            ...res.data.map<User>((follower) => {
-              return {
-                id: follower.id,
-                login: follower.login,
-              };
-            }),
-            ...data,
-          ];
-
-          currentPage++;
-        }
-      } catch (e: any) {
-        break;
-      }
+            App.Listeners.emit("result", compareResult);
+        } catch (e: any) {}
     }
 
-    return data;
-  }
+    async request(url: "GET /user/followers" | "GET /user/following") {
+        let currentPage = 1;
 
-  async compareFollowingAndFollowers() {
-    try {
-      if (!this.followers || !this.following) {
-        throw new Error("Cannot find followers or following. Please retry.");
-      }
+        const maxPage = this.configService.get<number>("MAX_PAGE");
+        let data: Array<User> = [];
 
-      const following = this.following.map((following) => following.id);
-      const followers = this.followers.map((follower) => follower.id);
+        while (currentPage <= maxPage) {
+            try {
+                const res = await this.octokit.request(url, {
+                    DEFAULT_OCTOKIT_HEADER,
+                    per_page: PER_PAGE,
+                    page: currentPage,
+                });
 
-      // 내가 팔로우 하고 있는 사람들 중에서 나를 팔로우 하지 않는 사람들
-      const NOT_FOLLOWING_BACK = this.following.filter(
-        (following) => !followers.includes(following.id)
-      );
+                if (res.status === HTTP_STATUS_OK) {
+                    data = [
+                        ...res.data.map<User>((follower) => {
+                            return {
+                                id: follower.id,
+                                login: follower.login,
+                            };
+                        }),
+                        ...data,
+                    ];
 
-      // 나를 팔로우 하고 있는 사람들 중에서 내가 팔로우 하지 않는 사람들
-      const NOT_FOLLOWING = this.followers.filter(
-        (follower) => !following.includes(follower.id)
-      );
+                    currentPage++;
+                }
+            } catch (e: any) {
+                break;
+            }
+        }
 
-      return {
-        notFollowingBack: NOT_FOLLOWING_BACK,
-        notFollowing: NOT_FOLLOWING,
-      };
-    } catch (e: any) {}
-  }
+        return data;
+    }
+
+    async compareFollowingAndFollowers() {
+        try {
+            if (!this.followers || !this.following) {
+                throw new Error(
+                    "Cannot find followers or following. Please retry."
+                );
+            }
+
+            const following = this.following.map((following) => following.id);
+            const followers = this.followers.map((follower) => follower.id);
+
+            // 내가 팔로우 하고 있는 사람들 중에서 나를 팔로우 하지 않는 사람들
+            const NOT_FOLLOWING_BACK = this.following.filter(
+                (following) => !followers.includes(following.id)
+            );
+
+            // 나를 팔로우 하고 있는 사람들 중에서 내가 팔로우 하지 않는 사람들
+            const NOT_FOLLOWING = this.followers.filter(
+                (follower) => !following.includes(follower.id)
+            );
+
+            return {
+                notFollowingBack: NOT_FOLLOWING_BACK,
+                notFollowing: NOT_FOLLOWING,
+            };
+        } catch (e: any) {}
+    }
 }
--- src/core/config-service.ts
+++ src/core/config-service.ts
@@ -10,24 +10,24 @@ const __ = Symbol.for("__ENV__");
 
 @injectable()
 export class ConfigService {
-  [__] = load({
-    GITHUB_TOKEN: String,
-    MAX_PAGE: Number,
-  }) as ENVMAP;
+    [__] = load({
+        GITHUB_TOKEN: String,
+        MAX_PAGE: Number,
+    }) as ENVMAP;
 
-  constructor() {
-    if (!this[__].GITHUB_TOKEN) {
-      throw new Error(
-        "Please set the environment variable named GITHUB_TOKEN in your .env file."
-      );
+    constructor() {
+        if (!this[__].GITHUB_TOKEN) {
+            throw new Error(
+                "Please set the environment variable named GITHUB_TOKEN in your .env file."
+            );
+        }
     }
-  }
 
-  load() {
-    return this[__];
-  }
+    load() {
+        return this[__];
+    }
 
-  get<T = string | number>(key: ENVKEY) {
-    return this[__][key] as T;
-  }
+    get<T = string | number>(key: ENVKEY) {
+        return this[__][key] as T;
+    }
 }
